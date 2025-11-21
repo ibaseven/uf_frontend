7c9479557ee16f3500fbcd74755f1e35f379dfc5
taharka200050-admin <sammbasow1999@gmail.com>
2025-11-21 17:21:37 +0000

    Add the logic of the backend

--- src/app/dashboard/mypurchase/page.tsx
+++ src/app/dashboard/mypurchase/page.tsx
@@ -1,10 +1,11 @@
+import { fetchJSON } from '@/lib/api';
 import BuyActionsView from "./_components/ByActions";
+import { GET_ACTION_PRICE_URL } from '@/lib/endpoint';
 
-
-
-
-const BuyActionsPage = () => {
-  return <BuyActionsView />;
+const BuyActionsPage = async () => {
+  const response = await fetchJSON(GET_ACTION_PRICE_URL);  
+  const pricePerAction = response.pricePerAction
+  return <BuyActionsView pricePerAction={pricePerAction} />;
 };
 
 export default BuyActionsPage;
